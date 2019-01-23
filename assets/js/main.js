326695bea23866efbb5cd45d4b84c1eab208de74
VovaNguyen11 <vovanguyen11@gmail.com>
2019-01-23 18:31:47 +0200

    done hero section

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,9 @@
 (function ($) {
     $(document).ready(function () {
-
+        //Slick slider
+        $('.js-hero-slider').slick({
+            arrows: true,
+            draggable: false
+        });
     })
 })(jQuery);
