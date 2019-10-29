71e08d6438e1186498739fe825b98dd7ba98b76a
VovaNguyen11 <vovanguyen11@gmail.com>
2019-10-29 15:58:03 +0700

    done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,35 @@
-(function ($) {
-    $(document).ready(function () {
+(function($) {
+    $(document).ready(function() {
         //Slick slider
-        $('.js-hero-slider').slick({
+        $(".js-hero-slider").slick({
             arrows: true,
             draggable: false
         });
-    })
+
+        const $list = $(".js-works").isotope({
+            itemSelector: ".works__item",
+            layoutMode: "masonry",
+            percentPosition: true,
+            masonry: {
+                columnWidth: ".grid-sizer"
+            }
+        });
+        $(".filter").on("click", ".filter__item", function() {
+            const $this = $(this);
+            const $filterValue = $this.data("filter");
+            $list.isotope({
+                filter: $filterValue
+            });
+        });
+
+        const filterItemClassName = "filter__item";
+        const $filterItem = $(`.${filterItemClassName}`);
+        $filterItem.first().addClass(`${filterItemClassName}--active`);
+        $filterItem.click(function(e) {
+            $filterItem
+                .removeClass(`${filterItemClassName}--active`)
+                .eq($(this).index())
+                .addClass(`${filterItemClassName}--active`);
+        });
+    });
 })(jQuery);
